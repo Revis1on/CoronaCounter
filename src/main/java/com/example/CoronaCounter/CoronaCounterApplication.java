f78fd5916a8f1d4dcf25d47b227425c8fd7d9042
Filip Risteski <filip22risteski@gmail.com>
2021-04-28 22:41:52 +0200

    version 0.3

---
+++ src/main/java/com/example/CoronaCounter/CoronaCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoronaCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoronaCounter/CoronaCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoronaCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
